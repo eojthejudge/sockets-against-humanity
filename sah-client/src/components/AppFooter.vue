d54fcba5304a83423d402e1b59f965b814458223
thejudge <thejudge@fortress.ch>
2021-05-09 09:58:36 +0200

    added footer

---
+++ sah-client/src/components/AppFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer>
+    <p>Copyright &copy; {{ year }}</p>
+  </footer>
+</template>
+<script>
+export default {
+  computed: {
+    year() {
+      const year = new Date().getFullYear();
+      return  year;
+    },
+  },
+};
+</script>
